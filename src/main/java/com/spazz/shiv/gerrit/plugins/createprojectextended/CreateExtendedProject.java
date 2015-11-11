4d0a411a1a12cae19c78797a0796710f64cad685
shiv <sprasad0603@gmail.com>
2015-11-11 02:36:34 -0800

    Added create project capability requirement. Added check for null/incorrect user. Also effectively ignored extended project features unless create_empty_commit is passed in true as input

--- src/main/java/com/spazz/shiv/gerrit/plugins/createprojectextended/CreateExtendedProject.java
+++ src/main/java/com/spazz/shiv/gerrit/plugins/createprojectextended/CreateExtendedProject.java
@@ -1,12 +1,17 @@
 package com.spazz.shiv.gerrit.plugins.createprojectextended;
 
+import com.google.gerrit.common.data.GlobalCapability;
+import com.google.gerrit.extensions.annotations.CapabilityScope;
 import com.google.gerrit.extensions.annotations.PluginName;
+import com.google.gerrit.extensions.annotations.RequiresCapability;
 import com.google.gerrit.extensions.api.GerritApi;
 import com.google.gerrit.extensions.api.projects.ProjectInput;
 import com.google.gerrit.extensions.restapi.*;
+import com.google.gerrit.server.CurrentUser;
 import com.google.gerrit.server.config.ConfigResource;
 import com.google.inject.Inject;
 
+import com.google.inject.Provider;
 import com.google.inject.assistedinject.Assisted;
 import com.spazz.shiv.gerrit.plugins.createprojectextended.CreateExtendedProject.ExtendedProjectInput;
 import org.slf4j.Logger;
@@ -17,7 +22,9 @@ import java.util.List;
 /**
  * Created by shivneil on 11/8/15.
  */
+@RequiresCapability(value = GlobalCapability.CREATE_PROJECT, scope = CapabilityScope.CORE)
 class CreateExtendedProject implements RestModifyView<ConfigResource, ExtendedProjectInput> {
+
     private final static Logger log = LoggerFactory.getLogger(CreateExtendedProject.class);
 
     static class ExtendedProjectInput extends ProjectInput {
@@ -31,16 +38,19 @@ class CreateExtendedProject implements RestModifyView<ConfigResource, ExtendedPr
 
     private String pluginName;
     private String name;
+    private final Provider<CurrentUser> currentUserProvider;
     private final GerritApi api;
 
     @Inject
     CreateExtendedProject(@PluginName String pluginName,
                           @Assisted String name,
+                          Provider<CurrentUser> currentUserProvider,
                           GerritApi api) {
         log.info("Constructor::hey it fired!");
 
         this.pluginName = pluginName;
         this.name = name;
+        this.currentUserProvider = currentUserProvider;
         this.api = api;
     }
 
@@ -49,33 +59,48 @@ class CreateExtendedProject implements RestModifyView<ConfigResource, ExtendedPr
             throws RestApiException {
         log.info("apply::hey it fired!");
 
+        // Make sure we have a valid user
+        CurrentUser user = currentUserProvider.get();
+        if(user == null || !user.isIdentifiedUser()) {
+            throw new AuthException("Authentication required");
+        }
+
+        // Let's not complicate things
         if(extendedProjectInput.name != null  && !extendedProjectInput.name.matches(name)) {
             throw new BadRequestException("name must match URL");
         }
 
+        // Create the project through the Gerrit REST API
+        ExtendedProjectInfo info = new ExtendedProjectInfo();
+        try {
+            info.projectInfo = api.projects().name(name).create(extendedProjectInput).get();
+
+        } catch (RestApiException rae) {
+            log.error(rae.getMessage());
+            throw rae;
+        }
+
+        // Only check extended input params if user wants to create empty commits.
+        // Should maintain compatibility with official API
         if(extendedProjectInput.createEmptyCommit) {
+
             // Do GitReview stuff
             StringBuilder sb = new StringBuilder("GitReview:")
                     .append(extendedProjectInput.gitreview);
+            info.gitreviewCommit = "b2m3nc: Added default .gitreview file";
 
             // Do GitIgnore stuff
             if (extendedProjectInput.gitignoreTemplates != null) {
                 sb.append(", GitIgnore Templates:");
+                info.gitignoreCommit = "a09s8d: Added .gitignore file";
+
                 for (String template : extendedProjectInput.gitignoreTemplates) {
                     sb.append(" ").append(template);
                 }
             }
             log.info("Extended Project Arguments: " + sb.toString());
         }
-        ExtendedProjectInfo info = new ExtendedProjectInfo();
-        try {
-            info.projectInfo = api.projects().name(name).create(extendedProjectInput).get();
-            info.gitignoreCommit = "a09s8d: Added .gitignore file";
-            info.gitreviewCommit = "b2m3nc: Added default .gitreview file";
-        } catch (RestApiException rae) {
-            log.error(rae.getMessage());
-            throw rae;
-        }
+
 
         return Response.created(info);
     }
